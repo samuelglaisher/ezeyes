aae3903da8e934df600d5a585bf44ccfb02108ac
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-02-06 14:58:19 -0500

    Organize component tree and add additional components

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 import PanelViewport from "./components/PanelViewport";
-import SettingsMenu from './components/SettingsMenu';
 import KeybindingManager from "./components/KeybindingManager";
+import FocusManager from "./components/FocusManager";
 import { PanelProvider } from "./contexts/PanelContext";
 import { SettingsProvider } from './contexts/SettingsContext';
 import { PanelViewportProvider } from "./contexts/PanelViewportContext";
 import "./styles/index.css";
+import { MenuManagerProvider } from "./contexts/MenuManagerContext";
+import { Provider, darkTheme } from "@adobe/react-spectrum";
 
 const App: React.FC = () => {
   return (
-    <SettingsProvider>
-      <PanelProvider>
-        <PanelViewportProvider>
-        <SettingsMenu />
-        <PanelViewport />
-        <KeybindingManager />
-        </PanelViewportProvider>
-      </PanelProvider>
-    </SettingsProvider>
+    <Provider theme={darkTheme}>
+      <PanelViewportProvider>
+        <SettingsProvider>
+          <PanelProvider>
+            <MenuManagerProvider>
+            <PanelViewport />
+            <KeybindingManager />
+            <FocusManager />
+            </MenuManagerProvider>
+          </PanelProvider>
+        </SettingsProvider>
+      </PanelViewportProvider>
+    </Provider>
   );
 };
 
