10f4c3e3d623b16ada4e59726aa77c89f84bec28
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-02-06 14:59:08 -0500

    Refactor components into logical header-main-footer structure. Add support for opening menus

--- src/react/components/PanelViewport.tsx
+++ src/react/components/PanelViewport.tsx
@@ -9,31 +9,55 @@ import { usePanelViewport } from "../hooks/usePanelViewport";
 import { PanelContext } from "../contexts/PanelContext";
 import { PanelViewportContext } from "../contexts/PanelViewportContext";
 import "../styles/index.css";
+import SettingsButton from "./SettingsButton";
+import { MenuManager } from './MenuManager'
+import { Button, ButtonGroup, Flex, Footer, Header } from "@adobe/react-spectrum";
+import ChevronDoubleRight from '@spectrum-icons/workflow/ChevronDoubleRight';
+import ChevronDoubleLeft from '@spectrum-icons/workflow/ChevronDoubleLeft';
+import Play from '@spectrum-icons/workflow/Play';
+import Pause from '@spectrum-icons/workflow/Pause';
+
 
 const PanelViewport: React.FC = () => {
     const { isPlaying } = useContext(PanelContext);
     const { activeView } = useContext(PanelViewportContext);
 
     const { togglePlayPause, navigateForward, navigateBackward} = usePanel();
-    const { switchView } = usePanelViewport();
 
     return (
-        <div>
+        <>
+            <Header margin="size-100">
+                <Flex justifyContent="center" width="100%">
+                    <SettingsButton />
+                </Flex>
+            </Header>
+
             <main>
                 <div id="panel-viewport">
-                    <button onClick={switchView}>Switch View</button>
+                    <MenuManager/>
                     {activeView === PanelDisplayType.HORIZONTAL && <HorizontalPanel />}
                     {activeView === PanelDisplayType.VERTICAL && <VerticalPanel />}
                     {activeView === PanelDisplayType.ZOOM && <ZoomView />}
                     {activeView === PanelDisplayType.FLASHCARD && <FlashcardView />}
                 </div>
             </main>
-            <footer>
-                <button onClick={navigateBackward}>Previous Sequence</button>
-                <button onClick={togglePlayPause}>{isPlaying ? 'Pause' : 'Play'}</button>
-                <button onClick={navigateForward}>Next Sequence</button>
-            </footer>
-        </div>
+
+            <Footer>
+                <Flex justifyContent="center" width="100%">
+                    <ButtonGroup justifySelf="center" alignSelf="center" align="center" margin="size-100">
+                        <Button variant="secondary" onPress={navigateBackward}>
+                            <ChevronDoubleLeft />
+                        </Button>
+                        <Button variant="secondary" onPress={togglePlayPause} autoFocus>
+                            {isPlaying ? <Pause /> : <Play />}
+                        </Button>
+                        <Button variant="secondary" onPress={navigateForward} autoFocus>
+                            <ChevronDoubleRight />
+                        </Button>
+                    </ButtonGroup>
+                </Flex>
+            </Footer>
+        </>
     );
 };
 
