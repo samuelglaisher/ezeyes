b00b541ddb788b7db6b7d7387c76cff8e415ef7d
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-02-06 15:00:40 -0500

    Fix settings wpm field

--- src/react/hooks/usePanel.tsx
+++ src/react/hooks/usePanel.tsx
@@ -17,14 +17,14 @@ export const usePanel = () => {
     setCurrentWordIndex(prevIndex => Math.max(prevIndex - settings.panels.wordSequenceLength, 0));
   };
 
-  const speed = 1000 / (settings.panels.wpm / 60);
+  const speed = 1000 / (settings.panels.wpm.curWpm / 60);
   const { togglePlayPause } = usePlaybackControl(navigateForward, speed);
 
   useEffect(() => {
     const words = textContent.replace(/\n/g, ' ').split(' ').filter(word => word.length !== 0);
     const wordSequenceLength = settings.panels.wordSequenceLength;
     const highlightedWords = words.slice(currentWordIndex, currentWordIndex + wordSequenceLength).join(' ');
-    
+
     setCurWordSequence([highlightedWords]);
   }, [textContent, currentWordIndex, settings.panels.wordSequenceLength, setCurWordSequence]);
 
