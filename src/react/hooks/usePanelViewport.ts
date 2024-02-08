2f59383dfe93acf1f1dcf4f249df840aee6c1db0
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-02-08 17:12:19 -0500

    Update settings on panel view change

--- src/react/hooks/usePanelViewport.ts
+++ src/react/hooks/usePanelViewport.ts
@@ -2,11 +2,13 @@ import { useEffect, useContext } from 'react';
 import { PanelDisplayType } from '../SettingsSchema';
 import { PanelViewportContext } from '../contexts/PanelViewportContext';
 import { SettingsContext } from '../contexts/SettingsContext';
+import { useSettings } from './useSettings';
 
 export const usePanelViewport = () => {
     const { settings } = useContext(SettingsContext);
 
-    const { setActiveView } = useContext(PanelViewportContext);
+    const { activeView, setActiveView } = useContext(PanelViewportContext);
+    const { changePanelSetting } = useSettings();
 
     useEffect(() => {
         setActiveView(settings.panels.displayType);
@@ -18,6 +20,8 @@ export const usePanelViewport = () => {
             const nextIndex = (viewOrder.indexOf(prevView) + 1) % viewOrder.length;
             return viewOrder[nextIndex];
         });
+
+        changePanelSetting("displayType", activeView);
     };
 
     return { switchView };
