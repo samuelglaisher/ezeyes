74851eb11f52a615f7917f4eaac6551c7e04c36a
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-02-08 17:12:29 -0500

    Fix imports

--- tests/react/App.test.tsx
+++ tests/react/App.test.tsx
@@ -1,6 +1,5 @@
-// App.test.tsx
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from '../../src/react/App';
 
 describe('App component', () => {
