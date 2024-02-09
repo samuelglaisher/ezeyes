788842ccab90e44d43dfc904d4a0a6471e0e1f9e
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-02-08 19:58:54 -0500

    Add additional unit tests for the renderModalContent function to raise code coverage

--- tests/react/components/MenuManager.test.tsx
+++ tests/react/components/MenuManager.test.tsx
@@ -1,8 +1,13 @@
 
-import React from 'react';
-import { render } from '@testing-library/react';
-import { MenuManager } from '../../../src/react/components/MenuManager';
+import React, { useContext } from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { MenuManager, renderModalContent } from '../../../src/react/components/MenuManager';
 import { useMenuManager } from '../../../src/react/hooks/useMenuManager';
+import { MenuManagerContext, MenuType } from '../../../src/react/contexts/MenuManagerContext';
+import { Menu, Provider, defaultTheme } from '@adobe/react-spectrum';
+
+const mockCloseMenu = jest.fn();
 
 jest.mock('../../../src/react/hooks/useMenuManager', () => ({
   useMenuManager: jest.fn().mockReturnValue({
@@ -19,5 +24,26 @@ describe('MenuManager component', () => {
   it('renders without crashing', () => {
     const { container } = render(<MenuManager />);
     expect(container).toBeTruthy();
+  });
+});
+
+
+const TestMenuManager = (currentMenu: MenuType) => {
+  return render(
+    <Provider theme={defaultTheme}>
+      <MenuManagerContext.Provider value={{
+        currentMenu,
+        setCurrentMenu: jest.fn(),
+      }}>
+      <MenuManager />
+      </MenuManagerContext.Provider>
+    </Provider>
+  );
+};
+
+describe('renderModalContent', () => {
+  it('renders SettingsMenu when currentMenu is SETTINGS', () => {
+    TestMenuManager(MenuType.SETTINGS);
+    expect(screen.getByText('Settings')).toBeInTheDocument();
   });
 });
