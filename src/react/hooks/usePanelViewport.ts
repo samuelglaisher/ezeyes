8973726a5f2f03b18551babafd40b07457316581
mhurm00 <mhurm00@gmail.com>
2024-03-04 17:27:12 -0500

    fixed flip flashcard and navigate to word being bound to the same action (left click) Added "flip flashcard" as a keybinding Bug: you can flip flashcard even when you aren't on flashcard view

--- src/react/hooks/usePanelViewport.ts
+++ src/react/hooks/usePanelViewport.ts
@@ -1,13 +1,17 @@
 import { useEffect, useContext } from 'react';
 import { PanelDisplayType } from '../SettingsSchema';
-import { PanelViewportContext } from '../contexts/PanelViewportContext';
+import { PanelType, PanelViewportContext } from '../contexts/PanelViewportContext';
 import { SettingsContext } from '../contexts/SettingsContext';
 import { useSettings } from './useSettings';
 
+function cycleView<T>(prevView: T, viewList: Array<T>): T {
+    const nextIndex = (viewList.indexOf(prevView) + 1) % viewList.length;
+    return viewList[nextIndex];
+}
+
 export const usePanelViewport = () => {
     const { settings } = useContext(SettingsContext);
-
-    const { activeView, setActiveView } = useContext(PanelViewportContext);
+    var { activeView, setActiveView, setActiveFlashcard } = useContext(PanelViewportContext);
     const { changePanelSetting } = useSettings();
 
     useEffect(() => {
@@ -15,14 +19,15 @@ export const usePanelViewport = () => {
     }, [settings.panels.displayType]);
 
     const switchView = () => {
-        setActiveView(prevView => {
-            const viewOrder = [PanelDisplayType.HORIZONTAL, PanelDisplayType.VERTICAL, PanelDisplayType.ZOOM, PanelDisplayType.FLASHCARD];
-            const nextIndex = (viewOrder.indexOf(prevView) + 1) % viewOrder.length;
-            return viewOrder[nextIndex];
-        });
-
+        setActiveView(prevView => cycleView(prevView, [PanelDisplayType.HORIZONTAL, PanelDisplayType.VERTICAL, PanelDisplayType.ZOOM, PanelDisplayType.FLASHCARD]));
         changePanelSetting("displayType", activeView);
     };
 
-    return { switchView };
+    const flipFlashcard = () => {
+        // bugged: allows you to flip flashcard even when you're not on flashcard
+        // neither settings.panels.displayType nor activePanel update with calls to switchView
+        setActiveFlashcard(prevPanel => cycleView(prevPanel, [PanelType.READER, PanelType.TEXT_INPUT]))
+    };
+
+    return { switchView, flipFlashcard };
 };
