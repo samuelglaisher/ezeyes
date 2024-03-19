58701297f5271f0dc81ee21de4afc46e9bdb88d6
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-03-19 19:58:45 -0400

    Fix runtime error when setting initial active state

--- src/react/hooks/usePanelViewport.ts
+++ src/react/hooks/usePanelViewport.ts
@@ -1,4 +1,4 @@
-import { useEffect, useContext, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import { PanelDisplayType } from '../SettingsSchema';
 import { PanelType, PanelViewportContext } from '../contexts/PanelViewportContext';
 import { SettingsContext } from '../contexts/SettingsContext';
@@ -9,23 +9,19 @@ function cycleView<T>(prevView: T, viewList: Array<T>): T {
 }
 
 export const usePanelViewport = () => {
-    const [isInit, setIsInit] = useState(true);
     const { settings } = useContext(SettingsContext);
-    var { setActiveView, setActiveFlashcard } = useContext(PanelViewportContext);
+    const { setActiveView, setActiveFlashcard } = useContext(PanelViewportContext);
 
-    if (isInit) {
+    useEffect(() => {
         setActiveView(settings.ui.defaultDisplayType);
-        setIsInit(false);
-    }
+    }, [setActiveView, settings.ui.defaultDisplayType]);
 
     const switchView = () => {
         setActiveView(prevView => cycleView(prevView, [PanelDisplayType.HORIZONTAL, PanelDisplayType.VERTICAL, PanelDisplayType.ZOOM, PanelDisplayType.FLASHCARD]));
     };
 
     const flipFlashcard = () => {
-        // bugged: allows you to flip flashcard even when you're not on flashcard
-        // neither settings.panels.displayType nor activePanel update with calls to switchView
-        setActiveFlashcard(prevPanel => cycleView(prevPanel, [PanelType.READER, PanelType.TEXT_INPUT]))
+        setActiveFlashcard(prevPanel => cycleView(prevPanel, [PanelType.READER, PanelType.TEXT_INPUT]));
     };
 
     return { switchView, flipFlashcard };
