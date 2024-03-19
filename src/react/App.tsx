976f3ce7529010f152805533ea9d4b2e8f5f0d45
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-03-19 19:19:48 -0400

    Add custom UIProvider component. Restructure dependencies

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,36 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import PanelViewport from "./components/PanelViewport";
 import KeybindingManager from "./components/KeybindingManager";
 import FocusManager from "./components/FocusManager";
 import SystemErrorModal from "./components/SystemErrorModal";
 import { PanelProvider } from "./contexts/PanelContext";
-import { SettingsProvider } from './contexts/SettingsContext';
+import { SettingsContext, SettingsProvider } from './contexts/SettingsContext';
 import { PanelViewportProvider } from "./contexts/PanelViewportContext";
 import { MenuManagerProvider } from "./contexts/MenuManagerContext";
-import { Provider, lightTheme } from "@adobe/react-spectrum";
+import { Provider, darkTheme, lightTheme } from "@adobe/react-spectrum";
 import { ErrorBoundary } from "react-error-boundary";
 import { FileManagerProvider } from "./contexts/FileManagerContext";
 import "./styles/index.css";
+import UIProvider from "./components/UIProvider";
 
 const App: React.FC = () => {
   return (
-    <Provider theme={lightTheme}>
-      <ErrorBoundary FallbackComponent={SystemErrorModal}>
-      <PanelViewportProvider>
-      <PanelProvider>
-        <FileManagerProvider>        
-          <SettingsProvider>
-            <MenuManagerProvider>
-            <PanelViewport />
-            <KeybindingManager />
-            <FocusManager />
-            </MenuManagerProvider>
-          </SettingsProvider>
-        </FileManagerProvider>
-      </PanelProvider>
-      </PanelViewportProvider>
-      </ErrorBoundary>
-    </Provider>
+    <SettingsProvider>
+      <UIProvider>
+        <ErrorBoundary FallbackComponent={SystemErrorModal}>
+        <PanelViewportProvider>
+        <PanelProvider>
+          <FileManagerProvider>        
+              <MenuManagerProvider>
+              <PanelViewport />
+              <KeybindingManager />
+              <FocusManager />
+              </MenuManagerProvider>
+          </FileManagerProvider>
+        </PanelProvider>
+        </PanelViewportProvider>
+        </ErrorBoundary>
+      </UIProvider>
+    </SettingsProvider>
+
   );
 };
 
