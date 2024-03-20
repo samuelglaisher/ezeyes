cd9136820cc5eb912c9d904e0fa3d30843ff4e7d
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-03-20 15:22:19 -0400

    Add overlay support to UI

--- src/react/components/UIProvider.tsx
+++ src/react/components/UIProvider.tsx
@@ -1,7 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { SettingsContext } from '../contexts/SettingsContext';
 import { Provider } from "@adobe/react-spectrum";
-import { FilterType } from '../SettingsSchema';
 
 const UIProvider = ({ children }: { children: React.ReactNode }) => {
     const { settings, getThemeObject } = useContext(SettingsContext);
@@ -18,7 +17,29 @@ const UIProvider = ({ children }: { children: React.ReactNode }) => {
         sepia(${settings.ui.sepia})
     `;
 
-    document.body.style.filter = filter;
+    useEffect(() => {
+        document.body.style.filter = filter;
+
+        const styleId = 'irlen-overlay-style';
+        let styleTag = document.getElementById(styleId) || document.createElement('style');
+        styleTag.id = styleId;
+        styleTag.innerHTML = `
+            body::before {
+                content: '';
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100vw;
+                height: 100vh;
+                background-color: ${settings.ui.overlayColor};
+                pointer-events: none;
+                z-index: 9999;
+            }
+        `;
+        if (!document.getElementById(styleId)) {
+            document.head.appendChild(styleTag);
+        }
+    }, [filter, settings.ui.overlayColor]);
 
     return (
         <Provider theme={getThemeObject(settings.ui.theme)} scale={settings.ui.size}>
