7b4356711bfb9465f9d48354db96e2acd259c736
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-04-05 17:14:24 -0400

    Add keybind for navigating back to top

--- src/react/hooks/useKeybindings.ts
+++ src/react/hooks/useKeybindings.ts
@@ -12,7 +12,7 @@ const useKeybindings = () => {
     const { settings } = useContext(SettingsContext);
 
     const { promptAndLoadFile } = useFileManager();
-    const { navigateForward, navigateBackward, navigateToPrevParagraph, navigateToNextParagraph, navigateToPrevSentence, navigateToNextSentence, togglePlayPause } = usePanel();
+    const { navigateForward, navigateBackward, navigateToPrevParagraph, navigateToNextParagraph, navigateToPrevSentence, navigateToNextSentence, togglePlayPause, backToTop } = usePanel();
     const { switchView, flipFlashcard } = usePanelViewport();
     const { openMenu } = useMenuManager();
 
@@ -55,6 +55,8 @@ const useKeybindings = () => {
         Mousetrap.bind(settings.keybindings.importFile, promptAndLoadFile);
 
         Mousetrap.bind(settings.keybindings.flipFlashcard, flipFlashcard);
+
+        Mousetrap.bind(settings.keybindings.backToTop, backToTop);
 
         return () => {
             Mousetrap.unbind(settings.keybindings.nextWord);
