b575a2f908b8ba807b54b533a67e0c390e60fc39
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-04-20 13:00:13 -0400

    Simplify and fix the ReaderDisplayPanel tests

--- tests/react/components/ReaderDisplayPanel.test.tsx
+++ tests/react/components/ReaderDisplayPanel.test.tsx
@@ -2,23 +2,50 @@ import '@testing-library/jest-dom';
 import React from 'react';
 import ReaderDisplay from '../../../src/react/components/ReaderDisplayPanel';
 import { render } from '@testing-library/react';
-import { PanelContext } from '../../../src/react/contexts/PanelContext';
+import { PanelContext, PanelContextType } from '../../../src/react/contexts/PanelContext';
 import { screen } from '@testing-library/react';
 
-const mockContext = (curWordSequence: string[]) => ({
-  curWordSequence,
+
+const mockPanelContext: PanelContextType = {
+  curWordSequence: '',
   setCurWordSequence: jest.fn(),
   textContent: '',
   setTextContent: jest.fn(),
   isPlaying: false,
   setIsPlaying: jest.fn(),
-});
+  prevParagraphIndex: 0,
+  setPrevParagraphIndex: jest.fn(),
+  nextParagraphIndex: 0,
+  setNextParagraphIndex: jest.fn(),
+  prevSentenceIndex: 0,
+  setPrevSentenceIndex: jest.fn(),
+  nextSentenceIndex: 0,
+  setNextSentenceIndex: jest.fn(),
+  prevWordSequenceIndex: 0,
+  setPrevWordSequenceIndex: jest.fn(),
+  curWordSequenceIndex: 0,
+  setCurWordSequenceIndex: jest.fn(),
+  nextWordSequenceIndex: 0,
+  setNextWordSequenceIndex: jest.fn(),
+  formattedTextContent: <></>,
+  setFormattedTextContent: jest.fn(),
+  sentenceIndices: [],
+  setSentenceIndices: jest.fn(),
+  paragraphIndices: [],
+  setParagraphIndices: jest.fn(),
+  wordSequenceIndices: [],
+  setWordSequenceIndices: jest.fn(),
+  wordIndices: [],
+  setWordIndices: jest.fn(),
+  generateWordSequenceIndicesFromIndex: jest.fn(),
+};
+
 
 describe('ReaderDisplay', () => {
   it('renders the current word sequence', () => {
-    const testWordSequence = ['Hello', 'World'];
+    mockPanelContext.curWordSequence = 'Hello World';
     render(
-      <PanelContext.Provider value={mockContext(testWordSequence)}>
+      <PanelContext.Provider value={mockPanelContext}>
         <ReaderDisplay />
       </PanelContext.Provider>
     );
@@ -27,8 +54,9 @@ describe('ReaderDisplay', () => {
   });
 
   it('handles an empty word sequence by ensuring the text element is empty', () => {
+    mockPanelContext.curWordSequence = '';
     const { container } = render(
-      <PanelContext.Provider value={mockContext([])}>
+      <PanelContext.Provider value={mockPanelContext}>
         <ReaderDisplay />
       </PanelContext.Provider>
     );
