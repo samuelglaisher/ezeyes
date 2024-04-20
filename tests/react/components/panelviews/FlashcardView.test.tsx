b49d4b330008806d00e6feac90eb10c56f55bf17
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-04-20 13:02:35 -0400

    Fix FlashcardView unit tests

--- tests/react/components/panelviews/FlashcardView.test.tsx
+++ tests/react/components/panelviews/FlashcardView.test.tsx
@@ -2,6 +2,17 @@ import '@testing-library/jest-dom';
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import FlashcardView from '../../../../src/react/components/panelviews/FlashcardView';
+import userEvent from '@testing-library/user-event';
+import { PanelType, PanelViewportContext, PanelViewportContextType } from '../../../../src/react/contexts/PanelViewportContext';
+import { PanelDisplayType } from '../../../../src/react/SettingsSchema';
+import { mock } from 'node:test';
+
+const mockPanelViewportContext: PanelViewportContextType = {
+  activeView: PanelDisplayType.FLASHCARD,
+  activeFlashcard: PanelType.READER,
+  setActiveView: jest.fn(),
+  setActiveFlashcard: jest.fn(),
+};
 
 jest.mock('../../../../src/react/components/TextInputDisplayPanel', () => {
   return {
@@ -29,9 +40,16 @@ describe('FlashcardView', () => {
     expect(container.firstChild).not.toHaveClass('flipped');
   });
 
-  it('applies "flipped" class when clicked', () => {
-    const { container } = render(<FlashcardView />);
-    fireEvent.click(container.firstChild!);
-    expect(container.firstChild).toHaveClass('flipped');
+  it('applies "flipped" class when ReaderDisplayPanel is not in view', () => {
+    mockPanelViewportContext.activeFlashcard = PanelType.TEXT_INPUT
+
+    render(
+      <PanelViewportContext.Provider value={mockPanelViewportContext}>
+        <FlashcardView />
+      </PanelViewportContext.Provider>
+    );
+
+    const flashcardContainer = screen.getByTestId('flashcard-container');
+    expect(flashcardContainer).toHaveClass('flipped');
   });
 });
