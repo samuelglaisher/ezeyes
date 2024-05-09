a642fe2ed71cc47309cce33af14d948baca3d268
mhurm00 <mhurm00@gmail.com>
2024-05-09 03:05:16 -0400

    Wrote first draft for tutorial

--- src/react/components/HelpMenu.tsx
+++ src/react/components/HelpMenu.tsx
@@ -5,23 +5,18 @@ import {
   ButtonGroup,
   Button,
   Flex,
-  Slider,
-  Picker,
   Item,
   TabList,
   TabPanels,
   Tabs,
-  NumberField,
   View,
-  Grid,
 } from '@adobe/react-spectrum';
+import ArrowLeftIcon from '@spectrum-icons/workflow/ArrowLeft';
+import ArrowRightIcon from '@spectrum-icons/workflow/ArrowRight';
 
-import {ColorArea, ColorSlider} from '@react-spectrum/color';
-import {parseColor} from '@react-stately/color';
 import { SettingsContext } from '../contexts/SettingsContext';
 import { useMenuManager } from '../hooks/useMenuManager';
-import { PanelDisplayType, ThemeType, UISize, WPMAttribute, WPMType } from '../SettingsSchema';
-import { KeybindInput } from './KeybindInput';
+import type {Key} from '@adobe/react-spectrum';
 
 interface HelpMenuProps {
     onClose: () => void;
@@ -30,12 +25,16 @@ interface HelpMenuProps {
 function HelpMenu(props: HelpMenuProps) {
     const { settings, dispatch } = useContext(SettingsContext);
     const { closeMenu } = useMenuManager();
+    let [section, setSection] = React.useState<Key>('basics');
 
     return (
         <>
             <Heading>Tutorial</Heading>
             <Content>
-                <Tabs aria-label="Settings Tabs" width={500}>
+                <Tabs aria-label="Help Tabs" 
+                selectedKey={section}
+                onSelectionChange={setSection}
+                width={500}>
                     <TabList>
                         <Item key="basics">Basics</Item>
                         <Item key="extra">Other Features</Item>
@@ -44,32 +43,71 @@ function HelpMenu(props: HelpMenuProps) {
                     </TabList>
                     <TabPanels height="size-3600">
                         <Item key="basics">
-                            <View height="size-3600" overflow="auto">
+                            <View height="size-3000" overflow="auto">
                                 <Flex direction="column" gap="size-150">
-                                    lksadjlksad
+                                    Welcome to EZEyes! EZEyes is a reader tool intended to help people recovering from traumatic brain injuries. To use it, simply load in some text, hit the play button, and keep your focus on the words flashing at the center of the screen. By reducing eye movement, EZEyes makes reading easier and less tiring, so you can spend more time doing it.
+
+                                    <Heading>Loading Text</Heading>
+                                    To read from a file on your computer, go to the top left, hit "File", then "Load File". This will open your computer's file manager, where you can choose a Word doc, PDF, or text file to read from. Opening the file will load the text into the reader.
+
+                                    Alternatively, you can paste text you have copied if you have the text input panel selected.
+                                    <Heading>Reading Text</Heading>
+                                    To start reading, click the pause/unpause button at the bottom middle of the screen. Words will appear at the center of the panel at a fixed rate that can be modified through settings. 
                                 </Flex>
                             </View>
+                            <Flex position='absolute' bottom='0' gap="size-150">
+                                <Button variant='primary' onPress={e => setSection('extra')}>
+                                    Next Page
+                                    <ArrowRightIcon size='XXL'/>
+                                </Button>
+                            </Flex>
                         </Item>
                         <Item key="extra">
-                            <View height="size-3600" overflow="auto">
+                            <View height="size-3000" overflow="auto">
                                 <Flex direction="column" gap="size-150">
                                     asdjlksajlkd
                                 </Flex>
                             </View>
+                            <Flex position='absolute' bottom='0' gap="size-150">
+                                <Button variant='primary' onPress={e => setSection('basics')}>
+                                    <ArrowLeftIcon size='XXL'/>
+                                    Previous Page
+                                </Button>
+                                <Button variant='primary' onPress={e => setSection('keybinds')}>
+                                    Next Page
+                                    <ArrowRightIcon size='XXL'/>
+                                </Button>
+                            </Flex>
                         </Item>
                         <Item key="keybinds">
-                            <View height="size-3600" overflow="auto">
+                            <View height="size-3000" overflow="auto">
                                 <Flex direction="column" gap="size-150">
                                     asdlksajlkd
                                 </Flex>
                             </View>
+                            <Flex position='absolute' bottom='0' gap="size-150">
+                                <Button variant='primary' onPress={e => setSection('extra')}>
+                                    <ArrowLeftIcon size='XXL'/>
+                                    Previous Page
+                                </Button>
+                                <Button variant='primary' onPress={e => setSection('wizard')}>
+                                    Next Page
+                                    <ArrowRightIcon size='XXL'/>
+                                </Button>
+                            </Flex>
                         </Item>
                         <Item key="wizard">
-                            <View height="size-3600" overflow="auto">
+                            <View height="size-3000" overflow="auto">
                                 <Flex direction="column" gap="size-150">
                                     asdjkasdjasd
                                 </Flex>
                             </View>
+                            <Flex position='absolute' bottom='0' gap="size-150">
+                                <Button variant='primary' onPress={e => setSection('keybinds')}>
+                                    Previous Page
+                                    <ArrowLeftIcon size='XXL'/>
+                                </Button>
+                            </Flex>
                         </Item>
                     </TabPanels>
                 </Tabs>
