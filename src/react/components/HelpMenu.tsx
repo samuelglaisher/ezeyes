9c3c5627a21524f0930adbb69a251b56e57f60c3
mhurm00 <mhurm00@gmail.com>
2024-05-13 04:43:38 -0400

    First draft of tutorial - needs wizard, screenshots for explanation

--- src/react/components/HelpMenu.tsx
+++ src/react/components/HelpMenu.tsx
@@ -38,76 +38,67 @@ function HelpMenu(props: HelpMenuProps) {
                     <TabList>
                         <Item key="basics">Basics</Item>
                         <Item key="extra">Other Features</Item>
-                        <Item key="keybinds">Keybinds</Item>
-                        <Item key="wizard">Wizard</Item>
+                        <Item key="wizard">Customization</Item>
                     </TabList>
                     <TabPanels height="size-3600">
                         <Item key="basics">
-                            <View height="size-3000" overflow="auto">
-                                <Flex direction="column" gap="size-150">
-                                    Welcome to EZEyes! EZEyes is a reader tool intended to help people recovering from traumatic brain injuries. To use it, simply load in some text, hit the play button, and keep your focus on the words flashing at the center of the screen. By reducing eye movement, EZEyes makes reading easier and less tiring, so you can spend more time doing it.
+                            <View height="size-3600" overflow="auto">
+                                <Flex direction="column"  marginTop={'size-150'}>
+                                    Welcome to EZEyes! EZEyes is a reader tool made for people recovering from a traumatic brain injury. To use it, simply load in some text, hit the play button, and read the changing words at the center of the screen. By reducing eye movement, EZEyes makes reading easier and more convenient. This guide is here to help you get the most out of EZEyes - explaining features and things you can do to customize your experience.
 
-                                    <Heading>Loading Text</Heading>
-                                    To read from a file on your computer, go to the top left, hit "File", then "Load File". This will open your computer's file manager, where you can choose a Word doc, PDF, or text file to read from. Opening the file will load the text into the reader.
+                                    <Heading marginBottom={0}>Loading Text</Heading>
+                                    Before you start reading, you need to give EZEyes something to read from. You can either read from a local file on your computer or copy-paste text from somewhere else. To load a file, hit "File", then "Load File", then choose a Word doc, PDF, or text file (.txt) to read from. To read text from somewhere else, copy it (Ctrl+C on Windows), click on the reader panel, then paste it in (Ctrl+V on Windows).
 
-                                    Alternatively, you can paste text you have copied if you have the text input panel selected.
-                                    <Heading>Reading Text</Heading>
-                                    To start reading, click the pause/unpause button at the bottom middle of the screen. Words will appear at the center of the panel at a fixed rate that can be modified through settings. 
+                                    <Heading marginBottom={0}>Reading</Heading>
+                                    To start reading, click the pause button at the bottom middle of the screen. To stop, click the button again.
+
+                                    <Flex gap='size-150' marginTop={'size-150'}>
+                                        <Button variant='primary' onPress={e => setSection('extra')}>
+                                            Next Page
+                                            <ArrowRightIcon size='XXL'/>
+                                        </Button>
+                                    </Flex>
                                 </Flex>
                             </View>
-                            <Flex position='absolute' bottom='0' gap="size-150">
-                                <Button variant='primary' onPress={e => setSection('extra')}>
-                                    Next Page
-                                    <ArrowRightIcon size='XXL'/>
-                                </Button>
-                            </Flex>
                         </Item>
                         <Item key="extra">
-                            <View height="size-3000" overflow="auto">
-                                <Flex direction="column" gap="size-150">
-                                    asdjlksajlkd
+                            <View height="size-3600" overflow="auto">
+                                <Flex direction="column" marginTop={'size-150'}>
+                                    <Heading margin={0}>Navigation</Heading>
+                                    Click on a word in the reader panel to move the reader there. You can also click the buttons to the left and right of the play button to go forward or back one word.
+
+                                    <Heading marginBottom={0}>Import Previous</Heading>
+                                    If you close EZEyes and want to come back to an article later, you can get back to where you left off by hitting "File", "Import Previous" then the name of the file you loaded before.
+
+                                    <Heading marginBottom={0}>Shortcuts</Heading>
+                                    Most actions in EZEyes can be performed by hitting a key on your keyboard. For example, you can press "{settings.keybindings.openSettings}" to open the settings menu.
+
+                                    <Flex gap='size-150' marginTop={'size-150'}>
+                                        <Button variant='primary' onPress={e => setSection('basics')}>
+                                            <ArrowLeftIcon size='XXL'/>
+                                            Previous Page
+                                        </Button>
+                                        <Button variant='primary' onPress={e => setSection('wizard')}>
+                                            Next Page
+                                            <ArrowRightIcon size='XXL'/>
+                                        </Button>
+                                    </Flex>
                                 </Flex>
                             </View>
-                            <Flex position='absolute' bottom='0' gap="size-150">
-                                <Button variant='primary' onPress={e => setSection('basics')}>
-                                    <ArrowLeftIcon size='XXL'/>
-                                    Previous Page
-                                </Button>
-                                <Button variant='primary' onPress={e => setSection('keybinds')}>
-                                    Next Page
-                                    <ArrowRightIcon size='XXL'/>
-                                </Button>
-                            </Flex>
-                        </Item>
-                        <Item key="keybinds">
-                            <View height="size-3000" overflow="auto">
-                                <Flex direction="column" gap="size-150">
-                                    asdlksajlkd
-                                </Flex>
-                            </View>
-                            <Flex position='absolute' bottom='0' gap="size-150">
-                                <Button variant='primary' onPress={e => setSection('extra')}>
-                                    <ArrowLeftIcon size='XXL'/>
-                                    Previous Page
-                                </Button>
-                                <Button variant='primary' onPress={e => setSection('wizard')}>
-                                    Next Page
-                                    <ArrowRightIcon size='XXL'/>
-                                </Button>
-                            </Flex>
                         </Item>
                         <Item key="wizard">
-                            <View height="size-3000" overflow="auto">
-                                <Flex direction="column" gap="size-150">
-                                    asdjkasdjasd
+                            <View height="size-3600" overflow="auto">
+                                <Flex direction="column" marginTop={'size-150'}>
+                                    EZEyes is very customizable. Here are a few of the key options with explanations of what they do:
+                
+                                    <Flex gap='size-150' marginTop={'size-150'}>
+                                        <Button variant='primary' onPress={e => setSection('extra')}>
+                                            <ArrowLeftIcon size='XXL'/>
+                                            Previous Page
+                                        </Button>
+                                    </Flex>
                                 </Flex>
                             </View>
-                            <Flex position='absolute' bottom='0' gap="size-150">
-                                <Button variant='primary' onPress={e => setSection('keybinds')}>
-                                    Previous Page
-                                    <ArrowLeftIcon size='XXL'/>
-                                </Button>
-                            </Flex>
                         </Item>
                     </TabPanels>
                 </Tabs>
