1c51c048b68a197cad1bdb4b3fc49468e7a46d48
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-05-13 21:50:24 -0400

    Fix WPM type not getting updated when it should

--- src/react/hooks/usePlaybackControl.ts
+++ src/react/hooks/usePlaybackControl.ts
@@ -9,6 +9,7 @@ export const usePlaybackControl = (navigateForward: () => void) => {
 
   const intervalId = useRef(null);
   const wpmRef = useRef(settings.processing.wpm[settings.processing.wpm.type].current);
+  const typeRef = useRef(settings.processing.wpm.type);
 
   useEffect(() => {
     if (isPlaying) {
@@ -21,8 +22,16 @@ export const usePlaybackControl = (navigateForward: () => void) => {
   }, [isPlaying]);
 
   useEffect(() => {
-    wpmRef.current = settings.processing.wpm[settings.processing.wpm.type].current;
+    wpmRef.current = settings.processing.wpm[typeRef.current].current;
   }, [settings.processing.wpm]);
+
+  useEffect(() => {
+    typeRef.current = settings.processing.wpm.type;
+    wpmRef.current = settings.processing.wpm[typeRef.current].current;
+    if (isPlaying) {
+      setupWordSequencePlayback();
+    }
+  }, [settings.processing.wpm.type, isPlaying]);
 
   const setupWordSequencePlayback = () => {
     clearInterval(intervalId.current);
@@ -35,13 +44,13 @@ export const usePlaybackControl = (navigateForward: () => void) => {
   };
 
   const increaseSpeed = () => {
-    const type = settings.processing.wpm.type;
+    const type = typeRef.current;
     const newSpeed = wpmRef.current + 1;
     dispatch({ type: 'UPDATE_WPM_SETTING', wpmType: type, setting: WPMAttribute.CURRENT, value: newSpeed });
   };
 
   const decreaseSpeed = () => {
-    const type = settings.processing.wpm.type;
+    const type = typeRef.current;
     const newSpeed = wpmRef.current - 1;
     dispatch({ type: 'UPDATE_WPM_SETTING', wpmType: type, setting: WPMAttribute.CURRENT, value: newSpeed });
   };
