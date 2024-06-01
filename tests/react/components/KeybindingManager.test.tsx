1382b76c22ce10fb2e59204fe538b9b885a33f49
asdf57 <53836417+asdf57@users.noreply.github.com>
2024-06-01 10:47:34 -0400

    Remove unneeded test scaffolding

--- tests/react/components/KeybindingManager.test.tsx
+++ tests/react/components/KeybindingManager.test.tsx
@@ -1,35 +1,11 @@
-import React, { ReactNode, useEffect } from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import KeybindingManager from '../../../src/react/components/KeybindingManager';
-import * as usePlaybackControlModule from '../../../src/react/hooks/usePlaybackControl';
-import { SettingsContext } from '../../../src/react/contexts/SettingsContext';
-import { lightTheme } from '@adobe/react-spectrum';
-import { initialSettings } from '../../../src/react/SettingsSchema';
 
 jest.mock('../../../src/react/hooks/useKeybindings', () => ({
   __esModule: true,
   default: jest.fn(),
 }));
-
-interface MockSettingsProviderProps {
-  children: ReactNode;
-}
-
-const MockSettingsProvider: React.FC<MockSettingsProviderProps> = ({ children }) => {
-  const mockSettings = {
-    ...initialSettings,
-    keybindings: {
-      ...initialSettings.keybindings,
-      play: ' ',
-    },
-  };
-
-  return (
-    <SettingsContext.Provider value={{ settings: mockSettings, showSettingsMenu: false, setShowSettingsMenu: () => {}, dispatch: () => {}, getThemeObject: () => lightTheme }}>
-      {children}
-    </SettingsContext.Provider>
-  );
-};
 
 describe('KeybindingManager component', () => {
   it('calls useKeybindings hook', () => {
