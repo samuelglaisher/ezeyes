bede5cdcda4393710f7a8d0b4cd8b50b247b6262
mhurm00 <mhurm00@gmail.com>
2024-06-03 22:41:26 -0400

    "Completed" e2e tests (this file is a disaster)

--- tests/react/App.test.tsx
+++ tests/react/App.test.tsx
@@ -6,8 +6,12 @@ import App from '../../src/react/App';
 import userEvent from '@testing-library/user-event';
 import UIProvider from '../../src/react/components/UIProvider';
 import { PanelContext } from '../../src/react/contexts/PanelContext';
-
+import { SettingsContext } from '../../src/react/contexts/SettingsContext';
 var Mousetrap = require('mousetrap-pause')(require('mousetrap'));
+import { FileManagerContext, FileManagerContextType } from '../../src/react/contexts/FileManagerContext';
+import { Provider, darkTheme } from "@adobe/react-spectrum";
+import { SettingsContextType } from '../../src/react/contexts/SettingsContext';
+
 
 const sampleText = 'Last year\'s El Niño caused 37 hurricanes.\nMeteorologists say that this was "pretty crazy".';
 const sampleTextOneLine = 'Last year\'s El Niño caused 37 hurricanes. Meteorologists say that this was "pretty crazy".';
@@ -45,12 +49,15 @@ describe('End-To-End Tests', () => {
     expect(divElement).toBeInTheDocument();
   });
 
+
   test('render uiprovider', async () => {
     render(<UIProvider><div>{sampleTextOneLine}</div></UIProvider>);
     expect(screen.getByText(sampleTextOneLine)).toBeInTheDocument();
   });
 
+
   test('basic reader flow', async () => {
+    const reader = rendered.container.querySelector('#reader-display-panel .reader-text')
     const input = rendered.container.querySelector('#text-input-panel') || window;
     fireEvent.click(input);
     userEvent.paste(sampleText);
@@ -60,8 +67,9 @@ describe('End-To-End Tests', () => {
     let predictedTime = predictReaderDuration(tokens.length);
 
     Mousetrap.trigger(initialSettings.keybindings.play);
+
     setTimeout(function() {
-      expect(screen.getByText(lastWord)).toBeInTheDocument();
+      expect(reader).toHaveTextContent(lastWord);
     }, predictedTime);
   });
   
@@ -136,37 +144,95 @@ describe('End-To-End Tests', () => {
     expect(finalWPM).toBe(initialWPM);
   });
 
+  /*
   test('reader flow settings', async () => {
-    /*
-      Settings are at their defaults
-      1. With the app open, click on the text input panel.
-      2. Paste in text to read from. This should contain uppercase/lowercase letters, diacritics, numbers, punctuation, spaces and newlines.
-      3. Hit the pause/play button to start reading.
-      4. Pause the reader.
-      5. Open the settings and reduce the reader speed to 30 wpm (in the assist range), increase the reader size to 48px and text input to 24px, and change the forward/back paragraph keybindings to a and d.
-      6. Hit d to get back to what you missed.
-      The reader speed is lower, the reader/text input size is larger, there is an orange filter over the program, and the reader is back at the first word in the paragraph.
-    */
-  });
+    const mockSettingsContext: SettingsContextType = {
+      settings: {
+        processing: {
+          wpm: {
+            ...initialSettings.processing.wpm,
+            type: WPMType.ASSISTED,
+            assisted: { min: 10, max: 100, current: 30 },
+          },
+          wordSequenceLength: initialSettings.processing.wordSequenceLength,
+        },
+        ui: {
+          ...initialSettings.ui,
+          overlayColor: '#ffa50033',
+        },
+        textInputPanel: {
+          fontSize: 24,
+        },
+        readerPanel: {
+          fontSize: 48,
+        },
+        keybindings: {
+          ...initialSettings.keybindings,
+          prevParagraph: "a"
+        }
+      }, 
+      dispatch: jest.fn(),
+      showSettingsMenu: false,
+      setShowSettingsMenu: jest.fn(),
+      getThemeObject: jest.fn()
+    };
+    render(<SettingsContext.Provider value={mockSettingsContext}><App /></SettingsContext.Provider>)
 
-  test('reader flow previous', async () => {
-    /*
-      Settings are at their defaults
-      1. Open the app and import an article from local files.
-      2. Play the reader, then pause and close the application.
-      3. Reopen the application. 
-      4. Navigate to “Import Previous” and click the previously-opened file.
-      5. Once the file loads, scroll down the text input panel until you reach the word you paused at and click it.
-      The reader is in the same state as it was when initially pausing
-    */
+    const input = rendered.container.querySelector('#text-input-panel') || rendered.container;
+    fireEvent.click(input);
+    userEvent.paste(sampleText);
+
+    Mousetrap.trigger(initialSettings.keybindings.play);
+    setTimeout(() => {
+      Mousetrap.trigger(initialSettings.keybindings.play);
+    }, 1000);
+
+    Mousetrap.trigger(initialSettings.keybindings.prevParagraph);
+
+    const reader = rendered.container.querySelector('#reader-display-panel .reader-text') || rendered.container;
+    let readerFontSize = (reader as HTMLElement).style.fontSize;
+    expect(readerFontSize).toEqual('48px');
+
+    let inputFontSize = (input as HTMLElement).style.fontSize;
+    expect(inputFontSize).toEqual('24px');
+
+    const overlay = rendered.container.querySelector("body::before") || rendered.container;
+    let overlayColor = (overlay as HTMLElement).style.backgroundColor;
+    expect(overlayColor).toEqual('rgba(255, 165, 0, 0.2)');
+
+    let tokens = sampleText.split(' ');
+    expect(reader).toHaveTextContent(tokens[0]);
   });
+  */
 
   test('reader flow panel', async () => {
-    /*
-      Text is loaded into the application, on Zoom view
-      1. Hit change view keybinding 3 times to get to the horizontal view
-      2. Drag the separator down so the reader panel takes up more than 2/3 of the screen, then start the reader
-      The reader is in the horizontal position with the reader taking up 2/3 of the window.
-    */
+    const input = rendered.container.querySelector('#text-input-panel') || window;
+    fireEvent.click(input);
+    userEvent.paste(sampleText);
+
+    Mousetrap.trigger(initialSettings.keybindings.switchView);
+    Mousetrap.trigger(initialSettings.keybindings.switchView);
+    Mousetrap.trigger(initialSettings.keybindings.switchView);
+
+    const bar = rendered.container.querySelector('.resize-bar-horizontal') || window;
+    let containerHeight = rendered.container.querySelector('#panel-container')?.clientHeight || 0
+    let newPosition = containerHeight * 0.8
+
+    fireEvent.mouseDown(bar);
+    fireEvent.mouseMove(bar, { clientY: newPosition });
+    fireEvent.mouseUp(bar);
+
+    const readerHeight = rendered.container.querySelector('#reader-display-panel')?.clientHeight
+    const textInputHeight = rendered.container.querySelector('#text-input-panel')?.clientHeight
+
+    expect(readerHeight).toEqual(containerHeight * 0.8)
+    expect(textInputHeight).toEqual(containerHeight * 0.2)
   });
+
+  // settings change behavior
+  // reader flow previous
+  // reader flow error recovery
+  // reader flow break
+  // reader flow acceptance
+  // cut (low priority + time constraints)
 });
